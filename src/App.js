f91713fd2e9596158ea09ed772f5b15b60a0a46b
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-01-21 18:10:21 +0200

    [Update] Built with love tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Routes from "./routes";
 import UserProvider from "./providers/UserProvider";
+import { Button } from "antd";
 
 function App() {
   return (
@@ -7,6 +8,21 @@ function App() {
       <UserProvider>
         <Routes />
       </UserProvider>
+      <div
+        style={{
+          position: "absolute",
+          bottom: "16px",
+          right: "16px",
+          zIndex: "1000",
+        }}
+      >
+        Built with ❤️ by{" "}
+        <Button type="link" style={{ padding: 0 }}>
+          <a href="https://www.michaelparkadze.com/" target="_blank">
+            Michael Parkadze
+          </a>
+        </Button>
+      </div>
     </>
   );
 }
