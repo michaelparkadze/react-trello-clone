025a0af0586d747212885bec61955deda551a2cf
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-01-21 15:51:32 +0200

    [Update] UI changes

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,59 @@
+import { useState, useEffect } from "react";
 import { Draggable } from "react-beautiful-dnd";
+import { Button, Input } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import "./styles.scss";
 
 export default function Card(props) {
-  const { index, title, cardKey } = props;
+  const [showIcons, setShowIcons] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [cardTitle, setCardTitle] = useState("");
+
+  const {
+    index,
+    title,
+    cardKey,
+    listKey,
+    handleEditCard,
+    handleDeleteCard,
+  } = props;
+
+  useEffect(() => {
+    setCardTitle(title);
+  }, []);
+
+  const handleTitleChange = (e) => {
+    setCardTitle(e.target.value);
+  };
+
+  const handleShowIcons = () => {
+    setShowIcons(true);
+  };
+
+  const handleHideIcons = () => {
+    setShowIcons(false);
+  };
+
+  const handleEnableEditing = () => {
+    setEditing(true);
+  };
+
+  const handleDisableEditing = () => {
+    setEditing(false);
+  };
+
+  const handleSubmitForm = (event, callback, listKey, cardKey, title) => {
+    event.preventDefault();
+
+    callback(listKey, cardKey, { title: cardTitle }).then(() =>
+      setEditing(false)
+    );
+  };
+
+  const onDeleteCard = (callback, listKey, cardKey) => {
+    callback({ listKey, cardKey });
+  };
+
   return (
     <Draggable draggableId={String(cardKey)} index={index}>
       {(provided) => (
@@ -12,12 +63,58 @@ export default function Card(props) {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
-            // onDoubleClick={() => setIsEditing(true)}
-            onClick={() => {
-              console.log(cardKey);
-            }}
+            onMouseEnter={handleShowIcons}
+            onMouseLeave={handleHideIcons}
+            onBlur={handleDisableEditing}
           >
-            <div className="card-container__content">{title}</div>
+            <div className="card-container__content">
+              {editing ? (
+                <form
+                  onSubmit={(event) =>
+                    handleSubmitForm(
+                      event,
+                      handleEditCard,
+                      listKey,
+                      cardKey,
+                      title
+                    )
+                  }
+                >
+                  <Input
+                    value={cardTitle}
+                    onChange={(event) => handleTitleChange(event)}
+                    autoFocus
+                  />
+                </form>
+              ) : (
+                <>
+                  {showIcons && (
+                    <div
+                      className="card-icons"
+                      onClick={(event) => event.stopPropagation()}
+                    >
+                      <Button
+                        onClick={handleEnableEditing}
+                        icon={<EditOutlined />}
+                        style={{ fontSize: 8, border: "none" }}
+                      ></Button>
+                      <Button
+                        onClick={() =>
+                          onDeleteCard(handleDeleteCard, listKey, cardKey)
+                        }
+                        icon={<DeleteOutlined />}
+                        style={{ fontSize: 8, border: "none" }}
+                      ></Button>
+                    </div>
+                  )}
+                  <div>{title}</div>
+                </>
+              )}
+            </div>
+            {/* <div className="card-container__options">
+              <div className="edit"></div>
+              <div className="delete"></div>
+            </div> */}
           </div>
         </>
       )}
